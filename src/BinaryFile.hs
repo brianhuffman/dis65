a374d34fb733c32dea995bdb8597f23443afe9ec
Brian Huffman <huffman@galois.com>
2020-12-17 13:19:38 -0800

    Move forward-analysis code to new Dis65.Forward module.

--- src/BinaryFile.hs
+++ src/BinaryFile.hs
@@ -1,15 +1,11 @@
+module BinaryFile where
 
 import qualified Data.ByteString as B
 
-import Data.Bits
-import Data.Char (chr, ord)
+import Data.Char (ord)
 import Data.Word (Word8, Word16)
 import Data.Int (Int8)
 import Data.Array
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
 
 ascii :: String -> [Word8]
 ascii = map (fromIntegral . ord)
@@ -53,7 +49,7 @@ prettyBytes ws = "\n " ++ concatMap f xs ++ prettyBytes ys
         f x = " 0x" ++ showByte x ++ ","
 
 decodeBytes :: Word16 -> [Word8] -> String
-decodeBytes addr [] = ""
+decodeBytes _addr [] = ""
 decodeBytes addr (op : bytes) =
   case (opTable ! op, bytes) of
     (Imp n, xs) -> pre n ++ "         ; " ++ post [] xs
@@ -164,171 +160,3 @@ opTable = listArray (0x00, 0xff)
   , Imp "SED", Aby "SBC", Undoc    , Undoc
   , Undoc    , Abx "SBC", Abx "INC", Undoc
   ]
-
-----------------------------------------------------------------------
--- Disassembler
-
-type Memory = Word16 -> Maybe Word8
-
-type MemState = Map Word16 AddrState
-
-mark :: Word16 -> AddrState -> MemState -> MemState
-mark = Map.insertWith mappend
-
--- | A set of bit flags that indicate how a particular address is
--- used.
-newtype AddrState = AddrState Word8
-  deriving (Eq)
-
-instance Semigroup AddrState where
-  AddrState x <> AddrState y = AddrState (x .|. y)
-
-instance Monoid AddrState where
-  mempty = AddrState 0
-  mappend = (<>)
-
-has :: AddrState -> AddrState -> Bool
-has x y = mappend x y == y
-
-addrJump :: AddrState
-addrJump = AddrState (bit 0)
-
-addrExec :: AddrState
-addrExec = AddrState (bit 1)
-
-addrRead :: AddrState
-addrRead = AddrState (bit 2)
-
-addrWrite :: AddrState
-addrWrite = AddrState (bit 3)
-
-addrReadIx :: AddrState
-addrReadIx = AddrState (bit 4)
-
-addrWriteIx :: AddrState
-addrWriteIx = AddrState (bit 5)
-
-
-{-
-Memory map:
-
-As we process the file, we update another map of addresses.
-
-
-AddrState has various values, based on how that address has been used
-in the program:
-- execute
-- read
-- write
-- read with index
-- write with index
--}
-
-data OpType
-  = OpRel -- ^ Branch instructions
-  | OpJmp
-  | OpJsr
-  | OpEnd -- ^ RTS, RTI, BRK
-  | Op1 -- ^ 1-byte instructions
-  | Op2 -- ^ 2-byte instructions
-  | OpAbs AddrState
-  | OpUndoc
-
-{-
-    00  20  40  60  80  a0  c0  e0
-0c  nop BIT JMP JMP'STY LDY CPY CPX    $0000      absolute (6c is indirect JMP)
-0d  ORA AND EOR ADC STA LDA CMP SBC    $0000      absolute
-0e  ASL ROL LSR ROR STX LDX DEC INC    $0000      absolute (read-modify-write)
-19  ORA AND EOR ADC STA LDA CMP SBC    $0000,Y    absolute,Y
-1c  nop nop nop nop shy LDY nop nop    $0000,X    absolute,X
-1d  ORA AND EOR ADC STA LDA CMP SBC    $0000,X    absolute,X
-1e  ASL ROL LSR ROR shx LDX DEC INC    $0000,X    absolute,X (r-m-w) (absolute,Y shx/LDX)
-    1f  3f  5f  7f  9f  bf  df  ff
-
-Write: 
-RMW: xxxxx11x
-
--}
-
-
-opType :: Word8 -> OpType
-opType 0x00 = OpEnd
-opType 0x20 = OpJsr
-opType 0x40 = OpEnd
-opType 0x60 = OpEnd
-opType 0x4c = OpJmp
-opType x =
-  case opTable ! x of
-    Imp _ -> Op1
-    Imm _ -> Op2
-    Zpg _ -> Op2
-    Zpx _ -> Op2
-    Zpy _ -> Op2
-    Abs _ -> OpAbs a
-    Abx _ -> OpAbs a
-    Aby _ -> OpAbs a
-    Ind _ -> OpEnd
-    Inx _ -> Op2
-    Iny _ -> Op2
-    Rel _ -> OpRel
-    Undoc -> OpUndoc
-  where
-    a | x .&. 0xf0 == 0x80 = addrWrite
-      | x .&. 0xf0 == 0x90 = addrWriteIx
-      | x .&. 0x0f == 0x0e = mappend addrRead addrWrite
-      | x .&. 0x1f == 0x1e = mappend addrReadIx addrWriteIx
-      | x .&. 0x10 == 0x10 = addrReadIx
-      | otherwise          = addrRead
-
-alreadyExecuted :: MemState -> PC -> Bool
-alreadyExecuted s pc =
-  case Map.lookup pc s of
-    Just a | has addrExec a -> True
-    _ -> False
-
-readAddr :: Memory -> Word16 -> Maybe Word16
-readAddr mem x =
-  do lo <- mem x
-     hi <- mem (x + 1)
-     return (fromIntegral hi * 256 + fromIntegral lo)
-
-type PC = Word16
-
-loop ::
-  Memory ->
-  MemState ->
-  [PC] ->
-  MemState
-loop mem s [] = s
-loop mem s (pc : pcs)
-  | alreadyExecuted s pc = loop mem s pcs
-  | otherwise =
-      case mem pc of
-        Nothing -> loop mem s pcs -- jump to external code
-        Just x ->
-          case opType x of
-            OpRel ->
-              case mem (pc + 1) of
-                Nothing -> loop mem s pcs -- program crash
-                Just offset -> loop mem (mark pc' addrJump s') (pc2 : pc' : pcs)
-                  where pc2 = pc + 2
-                        pc' = addRel pc2 offset
-            OpJmp ->
-              case readAddr mem (pc + 1) of
-                Nothing -> loop mem s pcs -- program crash
-                Just pc' -> loop mem (mark pc' addrJump s') (pc' : pcs)
-            OpJsr ->
-              case readAddr mem (pc + 1) of
-                Nothing -> loop mem s pcs -- program crash
-                Just pc' -> loop mem (mark pc' addrJump s') (pc + 2 : pc' : pcs)
-            OpEnd -> loop mem s' pcs
-            Op1 -> loop mem s' (pc + 1 : pcs)
-            Op2 -> loop mem s' (pc + 2 : pcs)
-            OpAbs a ->
-              case readAddr mem (pc + 1) of
-                Nothing -> loop mem s pcs -- program crash
-                Just addr -> loop mem s'' (pc + 3 : pcs)
-                  where s'' = mark addr a s'
-            OpUndoc -> loop mem s pcs -- program crash
-  where
-    s' = mark pc addrExec s
---
+++ src/Dis65/Forward.hs
@@ -0,0 +1,235 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Dis65.Forward where
+
+import           Data.Bits
+import           Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import           Data.Maybe
+import           Data.Word
+
+import Dis65.Instruction
+
+--------------------------------------------------------------------------------
+-- * AddrState
+
+-- | A set of bit flags that indicate how a particular address is
+-- used.
+newtype AddrState = AddrState Word8
+  deriving (Eq)
+
+instance Semigroup AddrState where
+  AddrState x <> AddrState y = AddrState (x .|. y)
+
+instance Monoid AddrState where
+  mempty = AddrState 0
+  mappend = (<>)
+
+has :: AddrState -> AddrState -> Bool
+has x y = mappend x y == y
+
+addrJump :: AddrState
+addrJump = AddrState (bit 0)
+
+addrExec :: AddrState
+addrExec = AddrState (bit 1)
+
+addrRead :: AddrState
+addrRead = AddrState (bit 2)
+
+addrWrite :: AddrState
+addrWrite = AddrState (bit 3)
+
+addrReadIx :: AddrState
+addrReadIx = AddrState (bit 4)
+
+addrWriteIx :: AddrState
+addrWriteIx = AddrState (bit 5)
+
+addrReadInd :: AddrState
+addrReadInd = AddrState (bit 6)
+
+addrWriteInd :: AddrState
+addrWriteInd = AddrState (bit 7)
+
+--------------------------------------------------------------------------------
+-- * Forward analysis
+
+type Memory = IntMap Word8
+
+type MemState = IntMap AddrState
+
+type Addr = Int
+
+mark :: Addr -> AddrState -> MemState -> MemState
+mark = IntMap.insertWith mappend
+
+markRead :: AddrArg -> MemState -> MemState
+markRead =
+  \case
+    IndirectX z -> mark (fromIntegral z) addrReadInd
+    ZeroPage  z -> mark (fromIntegral z) addrRead
+    Immediate _ -> id
+    Absolute  w -> mark (fromIntegral w) addrRead
+    IndirectY z -> mark (fromIntegral z) addrReadInd
+    ZeroPageX z -> mark (fromIntegral z) addrReadIx
+    ZeroPageY z -> mark (fromIntegral z) addrReadIx
+    AbsoluteY w -> mark (fromIntegral w) addrReadIx
+    AbsoluteX w -> mark (fromIntegral w) addrReadIx
+
+markWrite :: AddrArg -> MemState -> MemState
+markWrite =
+  \case
+    IndirectX z -> mark (fromIntegral z) addrWriteInd
+    ZeroPage  z -> mark (fromIntegral z) addrWrite
+    Immediate _ -> id
+    Absolute  w -> mark (fromIntegral w) addrWrite
+    IndirectY z -> mark (fromIntegral z) addrWriteInd
+    ZeroPageX z -> mark (fromIntegral z) addrWriteIx
+    ZeroPageY z -> mark (fromIntegral z) addrWriteIx
+    AbsoluteY w -> mark (fromIntegral w) addrWriteIx
+    AbsoluteX w -> mark (fromIntegral w) addrWriteIx
+
+{-
+Memory map:
+
+As we process the file, we update another map of addresses.
+
+
+AddrState has various values, based on how that address has been used
+in the program:
+- execute
+- read
+- write
+- read with index
+- write with index
+-}
+
+data OpType
+  = OpRel -- ^ Branch instructions
+  | OpJmp
+  | OpJsr
+  | OpEnd -- ^ RTS, RTI, BRK
+  | Op1 -- ^ 1-byte instructions
+  | Op2 -- ^ 2-byte instructions
+  | OpAbs AddrState
+  | OpUndoc
+
+{-
+    00  20  40  60  80  a0  c0  e0
+0c  nop BIT JMP JMP'STY LDY CPY CPX    $0000      absolute (6c is indirect JMP)
+0d  ORA AND EOR ADC STA LDA CMP SBC    $0000      absolute
+0e  ASL ROL LSR ROR STX LDX DEC INC    $0000      absolute (read-modify-write)
+19  ORA AND EOR ADC STA LDA CMP SBC    $0000,Y    absolute,Y
+1c  nop nop nop nop shy LDY nop nop    $0000,X    absolute,X
+1d  ORA AND EOR ADC STA LDA CMP SBC    $0000,X    absolute,X
+1e  ASL ROL LSR ROR shx LDX DEC INC    $0000,X    absolute,X (r-m-w) (absolute,Y shx/LDX)
+    1f  3f  5f  7f  9f  bf  df  ff
+
+Write:
+RMW: xxxxx11x
+
+-}
+
+
+{-
+opType :: Word8 -> OpType
+opType 0x00 = OpEnd
+opType 0x20 = OpJsr
+opType 0x40 = OpEnd
+opType 0x60 = OpEnd
+opType 0x4c = OpJmp
+opType x =
+  case opTable ! x of
+    Imp _ -> Op1
+    Imm _ -> Op2
+    Zpg _ -> Op2
+    Zpx _ -> Op2
+    Zpy _ -> Op2
+    Abs _ -> OpAbs a
+    Abx _ -> OpAbs a
+    Aby _ -> OpAbs a
+    Ind _ -> OpEnd
+    Inx _ -> Op2
+    Iny _ -> Op2
+    Rel _ -> OpRel
+    Undoc -> OpUndoc
+  where
+    a | x .&. 0xf0 == 0x80 = addrWrite
+      | x .&. 0xf0 == 0x90 = addrWriteIx
+      | x .&. 0x0f == 0x0e = mappend addrRead addrWrite
+      | x .&. 0x1f == 0x1e = mappend addrReadIx addrWriteIx
+      | x .&. 0x10 == 0x10 = addrReadIx
+      | otherwise          = addrRead
+-}
+
+alreadyExecuted :: MemState -> Addr -> Bool
+alreadyExecuted s pc =
+  case IntMap.lookup pc s of
+    Just a -> has addrExec a
+    Nothing -> False
+
+loop ::
+  IntMap Instruction ->
+  MemState ->
+  [Addr] ->
+  MemState
+loop _mem s [] = s
+loop mem s (pc : pcs)
+  | alreadyExecuted s pc = loop mem s pcs
+  | otherwise =
+      case IntMap.lookup pc mem of
+        Nothing -> loop mem s pcs -- jump to external code/crash
+        Just instr ->
+          case instr of
+            Reg _ ->
+              loop mem s' (pc + 1 : pcs)
+            Stack _ ->
+              loop mem s' (pc + 1 : pcs)
+            Read _ arg ->
+              loop mem (markRead arg s') (pc + 1 + sizeAddrArg arg : pcs)
+            Write _ arg ->
+              loop mem (markWrite arg s') (pc + 1 + sizeAddrArg arg : pcs)
+            Modify _ arg ->
+              loop mem (markWrite arg (markRead arg s')) (pc + 1 + sizeAddrArg arg : pcs)
+            Accumulator _ ->
+              loop mem s' (pc + 1 : pcs)
+            Branch _ (fromIntegral -> target) ->
+              loop mem (mark target addrJump s') (pc + 2 : target : pcs)
+            BRK ->
+              loop mem s' pcs
+            JSR (fromIntegral -> target) ->
+              loop mem (mark target addrJump s') (pc + 3 : target : pcs)
+            RTI ->
+              loop mem s' pcs
+            RTS ->
+              loop mem s' pcs
+            AbsJMP (fromIntegral -> target) ->
+              loop mem (mark target addrJump s') (target : pcs)
+            IndJMP _ ->
+              loop mem s' pcs
+            Undoc _ ->
+              loop mem s' pcs
+
+          where
+            s' = mark pc addrExec s
+
+--------------------------------------------------------------------------------
+-- * Pretty printing
+
+ppAddrState :: AddrState -> String
+ppAddrState a =
+  unwords $
+  mapMaybe f
+  [ (addrJump, "Label")
+  , (addrExec, "PC")
+  , (addrRead, "R")
+  , (addrWrite, "W")
+  , (addrReadIx, "R+")
+  , (addrWriteIx, "W+")
+  , (addrReadInd, "(R)")
+  , (addrWriteInd, "(W)")
+  ]
+  where
+    f (x, s) = if has x a then Just s else Nothing
